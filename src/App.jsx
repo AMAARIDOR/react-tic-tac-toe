99cf2c1b2e7131a454197b4571863609cb148dcb
Amaari <arish2k3@gmail.com>
2023-04-15 15:20:37 +0530

    add squares and click handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
 import "./App.css";
+import { useState } from "react";
+
+function Square({ value }) {
+  function handleClick() {
+    console.log("clicked");
+  }
+  return (
+    <button onClick={handleClick} className="square">
+      {value}
+    </button>
+  );
+}
 
 function App() {
-  return <>Hello World</>;
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
 }
 
 export default App;
